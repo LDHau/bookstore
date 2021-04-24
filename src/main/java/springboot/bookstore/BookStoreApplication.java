6a00f2cd555169d4fbc6bf42bad0eb092fa5723a
DESKTOP-UUAQ27F\Hau <dienhau17@gmail.com>
2021-04-24 18:36:32 +0700

    update config hibernate

--- src/main/java/springboot/bookstore/BookStoreApplication.java
+++ src/main/java/springboot/bookstore/BookStoreApplication.java
@@ -1,11 +1,19 @@
 package springboot.bookstore;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, //
+        DataSourceTransactionManagerAutoConfiguration.class, //
+        HibernateJpaAutoConfiguration.class })
+@ComponentScan({"springboot.bookstore"})
 public class BookStoreApplication {
 
     public static void main(String[] args) {
